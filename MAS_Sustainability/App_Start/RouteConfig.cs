dac310607d2d9cbf4776850aee74b06c325dc5d5
NavodaGunasekera <52916477+NavodaGunasekera@users.noreply.github.com>
2019-08-30 22:13:21 +0530

    Add files via upload

--- MAS_Sustainability/App_Start/RouteConfig.cs
+++ MAS_Sustainability/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MAS_Sustainability
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "UserLogin", action = "Login", id = UrlParameter.Optional }
+                defaults: new { controller = "AddRepairTokens", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
